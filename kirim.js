bf53a751c397107dd45499562a15299ff81e13e8
RanzyahAdinata <122334993+RanzyahAdinata@users.noreply.github.com>
2023-11-01 07:27:14 +0700

    Add files via upload

---
+++ kirim.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('X-Powered-By', 'Node.js');
+
+    res.writeHead(404, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js',
+    });
+
+    let body = [];
+
+    req
+        .on('data', chunk => {
+            body.push(chunk);
+        })
+        .on('end', () => {
+            body = Buffer.concat(body).toString();
+            console.log(body);
+        });
+
+    const data = JSON.stringify({
+        success: true,
+        error: 'Loh Ndak Ada Ta?',
+        data: null,
+    });
+
+    res.end(data);
+});
+
+const PORT = 8000;
+server.listen(PORT, () => console.log('Server running on port ${PORT}'));
